aea6b47d837eba30dba6266b7bc4948143302de6
Cream <cream@MacBook-Air-khxng-Cream.local>
2025-11-06 11:09:16 +0700

    add name and year

--- lib/screens/timetable.dart
+++ lib/screens/timetable.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 class TimeTablePage extends StatefulWidget {
   const TimeTablePage({super.key});
 
@@ -10,16 +9,103 @@ class TimeTablePage extends StatefulWidget {
 }
 
 class _TimeTablePageState extends State<TimeTablePage> {
+  String? selectedYear = '2/2567'; // ค่าเริ่มต้น
+  final List<String> years = [
+    '1/2567',
+    '2/2567',
+    '1/2568',
+    '2/2568',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 0,
         backgroundColor: const Color(0XFF00A9E0),
         systemOverlayStyle: const SystemUiOverlayStyle(
           statusBarColor: Colors.transparent,
-          statusBarIconBrightness: Brightness.light, // Android → สีขาว
-          statusBarBrightness: Brightness.dark, // iOS → สีขาว
+          statusBarIconBrightness: Brightness.light,
+          statusBarBrightness: Brightness.dark,
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text('ตารางเรียน', style: TextStyle(color: Colors.white)),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // ข้อมูลนักศึกษา
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'นายสมชาย ใจดี',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Color(0xFF2E2E2E),
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'สาขา วิทยาการคอมพิวเตอร์',
+                      style: TextStyle(
+                        color: Color(0xFF7A7A7A),
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+            
+                // Dropdown ปีการศึกษา
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'เลือกภาคการศึกษา',
+                      style: TextStyle(
+                        color: Color(0xFF7A7A7A),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 6),
+                    Container(
+                      height: 45,
+                      padding: const EdgeInsets.symmetric(horizontal: 12, ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.grey.shade400),
+                        color: Colors.white,
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton<String>(
+                          value: selectedYear,
+                          items: years.map((String year) {
+                            return DropdownMenuItem<String>(
+                              value: year,
+                              child: Text(
+                                year,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedYear = newValue;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
