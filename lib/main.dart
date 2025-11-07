c6a92ff1186d1286ab544a2a923d733355e4db96
FoamNR <100157001+FoamNR@users.noreply.github.com>
2025-11-07 17:33:25 +0700

    first page to login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:smartpsru/screens/mainpage.dart';
+//import 'package:smartpsru/screens/mainpage.dart';
+import 'package:smartpsru/screens/login.dart';
 
 
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       
-      home: const HomeMainPage(),
+      home: const LoginPage(),
     );
   }
 }
